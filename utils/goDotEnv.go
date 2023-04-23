8a76d92cc1a38be113ddb7b80ec083ebebec853c
vikhyat <vikhyat.bhatnagar@jumbotail.com>
2023-04-23 18:04:32 +0530

    added utility for .env files

---
+++ utils/goDotEnv.go
@@ -0,0 +1,19 @@
+package utils
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func GoDotEnvVariable(key string) string {
+
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
