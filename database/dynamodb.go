0bf0e1b843a9a1d7e1c022ec34693416d1bff5b9
vikhyat <vikhyat.bhatnagar@jumbotail.com>
2023-04-29 21:30:37 +0530

    improved formatting

--- database/dynamodb.go
+++ database/dynamodb.go
@@ -1,59 +1,64 @@
 package main
 
 import (
+	"github.com/Real-Dev-Squad/feature-flag-backend/utils"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"log"
 	"os"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/Real-Dev-Squad/feature-flag-backend/utils"
 )
 
-
-var Constants map[string]string  = map[string]string {
-	"ENV" : 				"ENVIRONMENT",
-	"DEV": 					"DEVELOPMENT",
-	"PROD":					"PRODUCTION",
-	"REGION": 				"AWS_REGION",
-	"ACCESS_KEY":			"AWS_ACCESS_KEY",
-	"SECRET_KEY":			"AWS_SECRET_KEY",
+var Constants map[string]string = map[string]string{
+	"ENV":        "ENVIRONMENT",
+	"DEV":        "DEVELOPMENT",
+	"PROD":       "PRODUCTION",
+	"REGION":     "AWS_REGION",
+	"ACCESS_KEY": "AWS_ACCESS_KEY",
+	"SECRET_KEY": "AWS_SECRET_KEY",
 }
 
-type AWSCredentials struct{
+type AWSCredentials struct {
 	AccessKey string
 	SecretKey string
-	Region string
+	Region    string
 }
 
 var db *dynamodb.DynamoDB
 
-func init(){
+func init() {
 
 	env, found := os.LookupEnv(Constants["ENV"])
 	if !found {
 		// load the env values using the `setUpEnv` function.
 		utils.setUpEnv()
-	} 
-	
+	}
+
 }
 
-func getAWSCredentials() *AWSCredentials{
+func getAWSCredentials() *AWSCredentials {
 
 	awsCredentials := new(AWSCredentials)
 	awsCredentials.Region = os.Getenv(Constants["REGION"])
 	awsCredentials.AccessKey = os.Getenv(Constants["ACCES_KEY"])
 	awsCredentials.SecretKey = os.Getenv(Constants["SECRET_KEY"])
-	
+
 	return awsCredentials
 }
 
 func createDynamoDB() *dynamodb.DynamoDB {
 	awsCredentials := getAWSCredentials()
 
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+
 	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String(awsCredentials.Region),
-		Credentials: credentials.NewStaticCredentials(awsCredentials.AccessKey,awsCredentials.SecretKey,""),
+		Region:      aws.String(awsCredentials.Region),
+		Credentials: credentials.NewStaticCredentials(awsCredentials.AccessKey, awsCredentials.SecretKey, ""),
 	})
 
 	if err != nil {
