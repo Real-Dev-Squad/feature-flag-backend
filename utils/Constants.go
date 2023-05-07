fae5d987079d51424ddffa7eded5ea4438617f72
vikhyat <vikhyat.bhatnagar@jumbotail.com>
2023-05-07 20:31:42 +0530

    Added Error utils code and constants

--- utils/Constants.go
+++ utils/Constants.go
@@ -9,3 +9,19 @@ var Constants map[string]string = map[string]string{
 	"SECRET_KEY":    "AWS_SECRET_KEY",
 	"FF_TABLE_NAME": "FEATURE_FLAG_TABLE_NAME",
 }
+
+var FeatureFlagStatus map[string]string = map[string]string{
+	"ENABLED":  "ENABLED",
+	"DISABLED": "DISABLED",
+}
+
+var FeatureFlagAttributes map[string]string = map[string]string{
+	"Id":          "Id",
+	"name":        "name",
+	"description": "description",
+	"createdAt":   "createdAt",
+	"createdBy":   "createdBy",
+	"updatedAt":   "updatedAt",
+	"updatedBy":   "updatedBy",
+	"status":      "status",
+}
--- utils/ErrorOutput.go
+++ utils/ErrorOutput.go
@@ -1,24 +1,56 @@
 package utils
 
 import (
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"log"
 	"net/http"
 )
 
-func ClientError(status int) (events.APIGatewayProxyResponse, error) {
+func ClientError(status int, body string) (events.APIGatewayProxyResponse, error) {
 
-	return events.APIGatewayProxyResponse{
-		Body:       http.StatusText(status),
+	resp := events.APIGatewayProxyResponse{
+		Body:       body,
 		StatusCode: status,
-	}, nil
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+	}
+
+	return resp, nil
+}
+func ServerError(err error) (events.APIGatewayProxyResponse, error) {
+	errMsg := ""
+	if err != nil {
+		errMsg = err.Error()
+	}
+	return events.APIGatewayProxyResponse{
+		Body:       errMsg,
+		StatusCode: http.StatusInternalServerError,
+	}, fmt.Errorf("Internal server error: %v", err)
 }
 
-func ServerError(err error) (events.APIGatewayProxyResponse, error) {
-	log.Println(err)
-
-	return events.APIGatewayProxyResponse{
-		Body:       http.StatusText(http.StatusInternalServerError),
-		StatusCode: http.StatusInternalServerError,
-	}, nil
+func DdbError(err error) {
+	if aerr, ok := err.(awserr.Error); ok {
+		switch aerr.Code() {
+		case dynamodb.ErrCodeProvisionedThroughputExceededException:
+			log.Println(dynamodb.ErrCodeProvisionedThroughputExceededException, aerr.Error())
+		case dynamodb.ErrCodeResourceNotFoundException:
+			log.Println(dynamodb.ErrCodeResourceNotFoundException, aerr.Error(), "is the aws error")
+		case dynamodb.ErrCodeRequestLimitExceeded:
+			log.Println(dynamodb.ErrCodeRequestLimitExceeded, aerr.Error())
+		case dynamodb.ErrCodeInternalServerError:
+			log.Println(dynamodb.ErrCodeInternalServerError, aerr.Error())
+		case dynamodb.ErrCodeConditionalCheckFailedException:
+			log.Println(dynamodb.ErrCodeConditionalCheckFailedException, aerr.Error())
+		case dynamodb.ErrCodeTransactionConflictException:
+			log.Println(dynamodb.ErrCodeInternalServerError, aerr.Error())
+		default:
+			log.Println(aerr.Error())
+		}
+	} else {
+		log.Println(err.Error())
+	}
 }
