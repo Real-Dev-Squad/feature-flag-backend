972819ebabdd565225154b3ee6120fb48b1c5ee5
vikhyat <vikhyat.bhatnagar@jumbotail.com>
2023-05-14 07:03:13 +0530

    Updated ddb error method

--- utils/ErrorOutput.go
+++ utils/ErrorOutput.go
@@ -4,10 +4,14 @@ import (
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"log"
 	"net/http"
 )
+
+type ValidationError struct {
+	Field string
+	Error string
+}
 
 func ClientError(status int, body string) (events.APIGatewayProxyResponse, error) {
 
@@ -34,22 +38,7 @@ func ServerError(err error) (events.APIGatewayProxyResponse, error) {
 
 func DdbError(err error) {
 	if aerr, ok := err.(awserr.Error); ok {
-		switch aerr.Code() {
-		case dynamodb.ErrCodeProvisionedThroughputExceededException:
-			log.Println(dynamodb.ErrCodeProvisionedThroughputExceededException, aerr.Error())
-		case dynamodb.ErrCodeResourceNotFoundException:
-			log.Println(dynamodb.ErrCodeResourceNotFoundException, aerr.Error(), "is the aws error")
-		case dynamodb.ErrCodeRequestLimitExceeded:
-			log.Println(dynamodb.ErrCodeRequestLimitExceeded, aerr.Error())
-		case dynamodb.ErrCodeInternalServerError:
-			log.Println(dynamodb.ErrCodeInternalServerError, aerr.Error())
-		case dynamodb.ErrCodeConditionalCheckFailedException:
-			log.Println(dynamodb.ErrCodeConditionalCheckFailedException, aerr.Error())
-		case dynamodb.ErrCodeTransactionConflictException:
-			log.Println(dynamodb.ErrCodeInternalServerError, aerr.Error())
-		default:
-			log.Println(aerr.Error())
-		}
+		log.Printf("Error code %d, Error message %s", aerr.Code(), aerr.Error())
 	} else {
 		log.Println(err.Error())
 	}
