36f697e413868af73c75dd5a71ceaf5a66666f36
vikhyat <vikhyat.bhatnagar@jumbotail.com>
2023-05-22 06:48:41 +0530

    removed Panics with Error and fix condition in ClientError

--- utils/goDotEnv.go
+++ utils/goDotEnv.go
@@ -3,14 +3,16 @@ package utils
 import (
 	"github.com/joho/godotenv"
 	"log"
+	"errors"
 )
 
-func SetUpEnv() {
+func SetUpEnv() { //this is not for production environment only for dev, as we cannot load .ENV file in lambda
 
 	err := godotenv.Load("../.env")
 
 	if err != nil {
-		log.Panic("Error loading .env file")
+		log.Println("Error loading env file")
+		ServerError(errors.New("Error loading env file"))
 	}
 	log.Println("loaded env var")
 }
