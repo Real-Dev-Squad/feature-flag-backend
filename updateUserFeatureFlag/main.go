c964c6ff7813c4da4a66d9a63814fb59e4d5e629
Mehul Kiran Chaudhari <55375534+MehulKChaudhari@users.noreply.github.com>
2024-08-06 22:38:15 +0530

    Feat/reset api limit (#114)
    * feat: reset limit in db
    
    * add: new limit in DB
    
    * add: reset the lambda concurrency to unreserved so the lambda's are in working state again
    
    * add: new lambda to reset the limit

--- updateUserFeatureFlag/main.go
+++ updateUserFeatureFlag/main.go
@@ -15,6 +15,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/go-playground/validator/v10"
 )
 
@@ -29,7 +30,7 @@ func processUpdateByIds(userId string, flagId string, requestBody utils.UpdateFe
 	db := database.CreateDynamoDB()
 
 	utils.CheckRequestAllowed(db, utils.ConcurrencyDisablingLambda)
-	
+
 	input := &dynamodb.UpdateItemInput{
 		TableName: aws.String(utils.FEATURE_FLAG_USER_MAPPING_TABLE_NAME),
 		Key: map[string]*dynamodb.AttributeValue{
